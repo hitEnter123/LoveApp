73a6d04f01fa93af6de60bbcc54b5fe34ac4fb6c
ChoLianJiet <lianjiet_choo95@hotmail.com>
2019-03-31 01:03:54 +0800

    Lesson 3: Love App with all the TODOs

--- app/src/main/java/com/loveapp/MainActivity.java
+++ app/src/main/java/com/loveapp/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO 1: Create EditText variables and link to XML
+
+        //TODO 2: Create Button variable and link to XML
+
+        //TODO 5: Create TextView variable and link to XML and add in logic to set text
+
+        //TODO 6: Set text scrollable if too long
+
+        //TODO 3: Create OnClickListener and add Logic
+
+        //TODO 4: Create new method to calculate love percentage
+
     }
 }
